fa28955d51d865596718721612865b1212082683
Vidhant Sharma <vidhantsharma@gmail.com>
2025-03-09 16:15:32 +0530

    added helper folder

---
+++ helper/shift_gt_image.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    # take gt image as input
+
+    # upshift the entire image by say 20 pixels
+
+    # save the modified upshifted image alongside the binary_mask and name it as predicted_binary_mask.png
+    pass

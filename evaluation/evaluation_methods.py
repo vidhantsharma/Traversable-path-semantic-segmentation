75fa4760f43a73bf2839f5cdccb3c47b83eeae80
Vidhant Sharma <vidhantsharma@gmail.com>
2025-03-09 15:54:46 +0530

    add evaluation folder

---
+++ evaluation/evaluation_methods.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+if __name__ == "__main__":
+    def __init__(self):
+        pass
+    
+    @property
+    def IoU_method(self):
+        pass
+
+    @property
+    def pixel_accuracy(self):
+        pass
+    
